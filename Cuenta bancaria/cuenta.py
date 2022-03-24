a920bb3d0fe1ebebc1668a97080da4a228dfef90
tereesaalvarez <alvdeporteresa@gmail.com>
2022-03-24 10:02:22 +0100

    commit

--- Cuenta bancaria/cuenta.py
+++ Cuenta bancaria/cuenta.py
@@ -1,12 +1,16 @@
 class cuenta:
-    def __init__(self,cuenta, dinero, saldo):
+    def __init__(self,cuenta, dinero, saldo, operacion):
         self.cuenta = cuenta
         self.dinero = dinero
         self.saldo = saldo
+        self.operacion = operacion
        
     def retirar_saldo(self,retirar, saldo, saldo_nuevo):
         
         retirar = int(input("Escriba la cantidad que desea retirar"))
         saldo_nuevo = saldo - retirar
         print(saldo_nuevo)
+
+    def ingresar_saldo(self, operacion):
+        operacion = input("Que operacion desea realizar: 1- Ingresar dinero 2- Retirar Dinero 3- Transferir dinero: ")
 
