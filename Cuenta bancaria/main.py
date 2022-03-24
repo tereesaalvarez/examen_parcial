a10373c2bb83bc1092e7b03e9cac665c7c408710
tereesaalvarez <alvdeporteresa@gmail.com>
2022-03-24 10:27:06 +0100

    commit

--- Cuenta bancaria/main.py
+++ Cuenta bancaria/main.py
@@ -1,13 +1,30 @@
 if __name__ == '__main__':
 
-    import random
     from cuenta import *
     from cuentaplazofijo import *
     from cuentavip import *
 
     ID = 234567
+    print("Su ID del banco es {}".format(ID))
+    
     Titular = "Jose Alvarez"
-    numerocuenta = random.randint(12)
+    print("El titular de la cuenta es {}".format(Titular))
+    
+    numerocuenta = random.randint(100000000000, 999999999999)
+    print("El numero de su cuenta es {}".format(numerocuenta))
+    
+    inicio = datetime(2015, 1, 30)
+    final =  datetime(2019, 5, 28)
+    fecha_apertura = inicio + (final - inicio) * random.random()
+    print("La fecha de apertura es {}".format(fecha_apertura))
+
+    inicio = datetime(2020, 1, 30)
+    final =  datetime(2025, 5, 28)
+    fecha_vencimiento = inicio + (final - inicio) * random.random()
+    print("La fecha de vencimiento es {}".format(fecha_vencimiento))
+
+
+
 
     saldo = 10000
     cuentas = input("En que cuenta quiere trabajar: 1- Cuenta corriente 2- Cuenta a plazo fijo 3- Cuenta vip: ")
@@ -15,11 +32,24 @@ if __name__ == '__main__':
 
     if cuentas == 1 :
         if operacion == 1:
-            ingresar = int(input("Cual es la cantidad que desea ingresar: "))
-            saldo = (saldo + ingresar).add(operacion)
+            saldo = 10000
+            ingresar = 575
+            saldo = (saldo + ingresar)
             print("Su saldo ahora es de {}".format(saldo))
         if operacion == 2:
             retirar = int(input("Cual es la cantidad que desea retirar: "))
+            saldo =  (saldo - retirar)
+
+    if cuenta == 2:
+        if operacion == 1:
+            print("Ha seleccionado ingresar dinero")
+
+        if operacion == 2:
+            print("En esta cuenta al retirar dinero se le puede penalizar con un 5%")
+
+        if operacion == 3:
+            print("Ha seleccionado transferir dinero")
+        
 
 
 
