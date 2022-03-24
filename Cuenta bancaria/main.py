f93fc11939664d7fd91bae38181098b435ef1091
tereesaalvarez <alvdeporteresa@gmail.com>
2022-03-24 10:02:13 +0100

    commit

--- Cuenta bancaria/main.py
+++ Cuenta bancaria/main.py
@@ -9,10 +9,13 @@ if __name__ == '__main__':
     cuentas = input("En que cuenta quiere trabajar: 1- Cuenta corriente 2- Cuenta a plazo fijo 3- Cuenta vip: ")
     operacion = input("Que operacion desea realizar: 1- Ingresar dinero 2- Retirar Dinero 3- Transferir dinero: ")
 
-    if cuenta == 1:
-        operacion = input("Que operacion desea realizar: 1- Ingresar dinero 2- Retirar Dinero 3- Transferir dinero: ")
+    if cuentas == 1 :
         if operacion == 1:
             ingresar = int(input("Cual es la cantidad que desea ingresar: "))
             saldo = (saldo + ingresar).add(operacion)
             print("Su saldo ahora es de {}".format(saldo))
+        if operacion == 2:
+            retirar = int(input("Cual es la cantidad que desea retirar: "))
+            
+
 
